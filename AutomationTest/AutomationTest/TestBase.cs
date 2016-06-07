a99b1102f616b672b5a179ae4b4a1e0688d29e84
Kevin Ricardo Sejin <kevinricar24@hotmail.com>
2016-06-07 00:17:30 -0500

    Create Solution AutomationTest

---
+++ AutomationTest/AutomationTest/TestBase.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Drawing.Imaging;
+using System.IO;
+
+namespace AutomationTest
+{
+    public abstract class TestBase
+    {
+
+
+    }
+}
